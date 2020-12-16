fa3f8e2e6d7b7b23a3f9a19bb9bc1d33eadd6386
besrour walid <walidbesrour@gmail.com>
2020-12-16 18:37:46 +0100

    Thymeleaf

--- src/main/java/com/mycompany/invoce/controller/web/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoce/controller/web/InvoiceControllerWeb.java
@@ -5,6 +5,7 @@ import com.mycompany.invoce.entity.Invoice;
 import com.mycompany.invoce.service.InvoiceServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InvoiceControllerWeb implements InvoiceControllerInterface {
@@ -27,5 +28,10 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
         invoice.setCustomerName(name);
 
         invoiceService.createInvoice(invoice);
+    }
+    @RequestMapping("walid")
+    public String displayHome(){
+        System.out.println("desplayhome methode");
+        return "index";
     }
 }
