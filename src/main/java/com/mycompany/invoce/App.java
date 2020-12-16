4bf774762f78a34344582e3f9de56a9e14942829
besrour walid <walidbesrour@gmail.com>
2020-12-16 13:37:07 +0100

    Pourquoi a t-on besoin de Spring ? final

--- src/main/java/com/mycompany/invoce/App.java
+++ src/main/java/com/mycompany/invoce/App.java
@@ -3,18 +3,24 @@ package com.mycompany.invoce;
 
 import com.mycompany.invoce.controller.InvoiceControllerInterface;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.invoce.controller.web","com.mycompany.invoce.service.prefix","com.mycompany.invoce.repository.database"})
+@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class App 
 {
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
        InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
         invoiceController.createInvoic();
 
